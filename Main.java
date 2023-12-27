c0460cf7cc43ef194f59989be0671164bb31afc1
Mikhail-Osokin <123121620+Mikhail-Osokin@users.noreply.github.com>
2023-12-28 00:04:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ import ToyShop.Toys;
 public class Main {
     
     public static void main(String[] args) {
-    ArrayList<Integer> idToys = new ArrayList<>();
+        ArrayList<Integer> idToys = new ArrayList<>();
     ArrayList<Integer> frequencyToys = new ArrayList<>();
     ArrayList<String> nameToys = new ArrayList<>(); 
     Queue<String> listOfToys = new PriorityQueue<String>(10);
@@ -38,43 +38,30 @@ public class Main {
     PriseToys.put(toy.getFrequency(),toy.getToy());
     listOfToys.add(toy.getToy());
     System.out.println(listOfToys);
-    System.out.println(toy.getToy());
-    TreeMap<Integer,String> treeMap = new TreeMap<>();
-    treeMap.putAll(PriseToys);
-    //for (Map.Entry<Integer,String> entry:treeMap.entrySet()){
-    //    int key = entry.getKey();
-    //    String value = entry.getValue();
-    //
-    //    System.out.println(key+";"+value);
-    //}
-    System.out.println(listOfToys);
+    
     try
     {
         FileWriter writer = new FileWriter("LogPriseToys.txt",true);
         for (int i = 0; i < 10; i++) { 
         int a = (int)(Math.random()*9)+1;
-        for (Map.Entry<Integer,String> entry:treeMap.entrySet()){
-        int key = entry.getKey();
-        String value = entry.getValue();
-        if (key == a & a == 1){        
-        writer.write("с вероятностью"+key+"выигрывает"+value);
+        if (a == 1){        
+        writer.write("с вероятностью"+a+"выигрывает"+PriseToys.get(a));
         writer.write("\n");
         }
-        else if (key == a & a < 2 | a == 2){
-        writer.write("с вероятностью"+key+"выигрывает"+value);
+        else if (a == 2){
+        writer.write("с вероятностью"+a+"выигрывает"+PriseToys.get(a));
         writer.write("\n");    
         }
-        else {
-        writer.write("с вероятностью"+key+"выигрывает"+value);
+        else if (a > 2){
+        writer.write("с вероятностью"+6+"выигрывает"+PriseToys.get(6));
         writer.write("\n");
         } 
     }
         }
-}
+
     catch (Exception e){
         System.out.println("Ошибка исполнения");
     }
 
-    //public void getLuckyToy ()
 }
 }
