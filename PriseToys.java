4af3a968580a120a0e6012c619b3e9d873d302d9
Mikhail-Osokin <123121620+Mikhail-Osokin@users.noreply.github.com>
2023-12-27 23:29:06 +0300

    Update PriseToys.java

--- PriseToys.java
+++ PriseToys.java
@@ -9,15 +9,6 @@ public class PriseToys extends Toys {
 public String getToy(){
     int a = (int)(Math.random()*9)+1;
     String b = "";
-    if (a == 1 & this.getFrequency() == a) {
-        b = this.getId() + "; " + this.getFrequency() + "; " + this.getName();
-    }
-    else if (a > 1 & a < 2 | a == 2 & this.getFrequency() == a) {
-        b = this.getId() + "; " + this.getFrequency() + "; " + this.getName();
-    }
-    else if (a>2 & this.getFrequency() == a) {
-        b = this.getId() + "; " + this.getFrequency() + "; " + this.getName();      
-    }
     return b;
 }
 }
